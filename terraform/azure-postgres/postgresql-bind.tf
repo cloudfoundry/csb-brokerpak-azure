e3e3e060bc40cc81ab01e19ec8874008ac438b80
Ernie Billing <ebilling@pivotal.io>
2020-04-21 17:51:16 -0700

    [#171704155] azure postgres

--- terraform/azure-postgres/postgresql-bind.tf
+++ terraform/azure-postgres/postgresql-bind.tf
@@ -12,64 +12,72 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-variable mysql_db_name { type = string }
-variable mysql_hostname { type = string }
-variable mysql_port { type = number }
+variable db_name { type = string }
+variable hostname { type = string }
+variable port { type = number }
 variable admin_username { type = string }
 variable admin_password { type = string }
 
-provider "mysql" {
-  endpoint = format("%s:%d", var.mysql_hostname, var.mysql_port)
-  username = var.admin_username
-  password = var.admin_password
+provider "postgresql" {
+  host            = var.hostname
+  port            = var.port
+  username        = var.admin_username
+  password        = var.admin_password
+  superuser       = false
+  database        = var.db_name
 }
 
 resource "random_string" "username" {
   length = 16
   special = false
+  number = false  
 }
 
 resource "random_password" "password" {
-  length = 31
+  length = 64
   override_special = "~_-."
   min_upper = 2
   min_lower = 2
   min_special = 2
 }    
 
-resource "mysql_user" "newuser" {
-  user               = random_string.username.result
-  plaintext_password = random_password.password.result
-  host = "%"
+resource "postgresql_role" "new_user" {
+  name     = random_string.username.result
+  login    = true
+  password = random_password.password.result
+  skip_reassign_owned = true
+  skip_drop_role = true
 }
 
-resource "mysql_grant" "newuser" {
-  user       = mysql_user.newuser.user
-  database   = var.mysql_db_name
-  host = mysql_user.newuser.host
-  privileges = ["ALL"]
+resource "postgresql_grant" "all_access" {
+  depends_on  = [ postgresql_role.new_user ]
+  database    = var.db_name
+  role        = random_string.username.result
+  schema      = "public"
+  object_type = "table"
+  privileges  = ["ALL"]
 }
 
 locals {
-  username = format("%s@%s", random_string.username.result, var.mysql_hostname)
+  username = format("%s@%s", random_string.username.result, var.hostname)
 }
 output username { value = local.username }
 output password { value = random_password.password.result }
 output uri { 
   value = format("%s://%s:%s@%s:%d/%s", 
-                  "mysql",
+                  "postgresql",
                   local.username, 
                   random_password.password.result, 
-                  var.mysql_hostname, 
-                  var.mysql_port,
-                  var.mysql_db_name) 
+                  var.hostname, 
+                  var.port,
+                  var.db_name) 
 }
 output jdbcUrl { 
   value = format("jdbc:%s://%s:%s/%s?user=%s\u0026password=%s\u0026verifyServerCertificate=true\u0026useSSL=true\u0026requireSSL=false\u0026serverTimezone=GMT", 
-                  "mysql",
-                  var.mysql_hostname, 
-                  var.mysql_port,
-                  var.mysql_db_name, 
+                  "postgresql",
+                  var.hostname, 
+                  var.port,
+                  var.db_name, 
                   local.username, 
                   random_password.password.result) 
 }  
