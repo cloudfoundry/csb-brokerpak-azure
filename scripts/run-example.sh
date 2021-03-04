3141c2354421ed0ee5179c9897a4415979bc9514
Ernie Billing <ebilling@pivotal.io>
2021-03-04 11:32:39 -0800

    fixed default authorized_networks, added run-examples.sh helper

---
+++ scripts/run-example.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+if [ $# -lt 1 ]; then
+    echo "Usage: ${0} <service name> [optional example name]"
+    exit 1
+fi
+
+SERVICE_NAME=${1}; shift
+
+SECURITY_USER_NAME=${SECURITY_USER_NAME:=aws-broker}
+SECURITY_USER_PASSWORD=${SECURITY_USER_PASSWORD:=aws-broker-pw}
+DOCKER_OPTS="--rm -v $(PWD):/brokerpak -w /brokerpak --network=host"
+CSB=cfplatformeng/csb
+
+docker run ${DOCKER_OPTS} \
+	-e SECURITY_USER_NAME \
+	-e SECURITY_USER_PASSWORD \
+	-e USER \
+	${CSB} client run-examples --service-name ${SERVICE_NAME}
+
