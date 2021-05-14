1260d5d3cb6cd25ec4b8e0981c1de5c8dc4deac9
George Blue <gblue@pivotal.io>
2021-05-14 23:15:45 +0100

    test: fix negative response test

--- acceptance-tests/cf-create-service-should-fail.sh
+++ acceptance-tests/cf-create-service-should-fail.sh
@@ -23,9 +23,8 @@ PLAN=$1; shift
 
 SERVICE_INSTANCE_NAME="${SERVICE}-${PLAN}-$$-f"
 
-create_service "${SERVICE}" "${PLAN}" "${SERVICE_INSTANCE_NAME}" "$@"
-
-if [ $? -ne 0 ]; then
+if ! create_service "${SERVICE}" "${PLAN}" "${SERVICE_INSTANCE_NAME}" "$@"
+then
   if cf service ${SERVICE_INSTANCE_NAME}; then 
     echo "Purging service instance..."
     cf purge-service-instance -f "${SERVICE_INSTANCE_NAME}"
