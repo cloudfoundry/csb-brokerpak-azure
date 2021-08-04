4c6f517bd008d11a99e5cd754e0890fb938ad6c7
George Blue <gblue@pivotal.io>
2021-08-04 17:58:31 +0100

    test: Ginkgo style test for PostgreSQL
    [#178886040](https://www.pivotaltracker.com/story/show/178886040)

---
+++ acceptance-tests/postgresql/postgresql_suite_test.go
@@ -0,0 +1,13 @@
+package postgresql_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestRedis(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "PostgreSQL Suite")
+}
