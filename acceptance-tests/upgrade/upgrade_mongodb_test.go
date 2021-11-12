499a9186820a423c63225a66d69dcc9be1e5314c
James Norman <normanja@vmware.com>
2021-11-12 09:03:40 +0000

    chore: add mongodb upgrade test
    [#180082672](https://www.pivotaltracker.com/story/show/180082672)

---
+++ acceptance-tests/upgrade/upgrade_mongodb_test.go
@@ -0,0 +1,73 @@
+package upgrade_test
+
+import (
+	"acceptancetests/apps"
+	"acceptancetests/helpers"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("UpgradeMongoTest", func() {
+	Context("When upgrading broker version", func(){
+		It("should continue to work", func() {
+			By("pushing latest released broker version")
+			brokerName := helpers.RandomName("csb-mssql-db")
+			serviceBroker := helpers.PushAndStartBroker(brokerName, releasedBuildDir)
+			defer serviceBroker.Delete()
+
+			By("creating a service instance")
+			databaseName := helpers.RandomName("database")
+			collectionName := helpers.RandomName("collection")
+			serviceInstance := helpers.CreateServiceFromBroker("csb-azure-mongodb", "small", brokerName, map[string]interface{}{
+				"db_name":         databaseName,
+				"collection_name": collectionName,
+				"shard_key":       "_id",
+			})
+			defer serviceInstance.Delete()
+
+			By("pushing the unstarted app twice")
+			appOne := helpers.AppPushUnstarted(apps.MongoDB)
+			appTwo := helpers.AppPushUnstarted(apps.MongoDB)
+			defer helpers.AppDelete(appOne, appTwo)
+
+			By("binding the apps to the MongoDB service instance")
+			serviceInstance.Bind(appOne)
+			serviceInstance.Bind(appTwo)
+
+			By("starting the apps")
+			helpers.AppStart(appOne, appTwo)
+
+			By("creating a document using the first app")
+			documentNameOne := helpers.RandomHex()
+			documentDataOne := helpers.RandomHex()
+			appOne.PUT(documentDataOne, "%s/%s/%s", databaseName, collectionName, documentNameOne)
+
+			By("getting the document using the second app")
+			got := appTwo.GET("%s/%s/%s", databaseName, collectionName, documentNameOne)
+			Expect(got).To(Equal(documentDataOne))
+
+			By("pushing the development version of the broker")
+			serviceBroker.Update(developmentBuildDir)
+
+			By("deleting bindings created before the upgrade")
+			serviceInstance.Unbind(appOne)
+			serviceInstance.Unbind(appTwo)
+
+			By("creating new bindings")
+			serviceInstance.Bind(appOne)
+			serviceInstance.Bind(appTwo)
+			helpers.AppRestage(appOne, appTwo)
+
+			By("creating a document using the first app - post upgrade")
+			documentNameTwo := helpers.RandomHex()
+			documentDataTwo := helpers.RandomHex()
+			appOne.PUT(documentDataTwo, "%s/%s/%s", databaseName, collectionName, documentNameTwo)
+
+			By("getting the document using the second app")
+			got = appTwo.GET("%s/%s/%s", databaseName, collectionName, documentNameTwo)
+			Expect(got).To(Equal(documentDataTwo))
+
+
+		})
+	})
+})
