2e978c5e441c238b01ad719511a8cd233eb132c8
James Norman <normanja@vmware.com>
2021-12-10 09:04:28 +0000

    test: all env vars must be set within the same helper function
    [#180343101](https://www.pivotaltracker.com/story/show/180343101)

--- acceptance-tests/mssql-db-failover-group/mssql_fog_db_subsume_test.go
+++ acceptance-tests/mssql-db-failover-group/mssql_fog_db_subsume_test.go
@@ -55,9 +55,8 @@ var _ = Describe("MSSQL Failover Group DB Subsume", func() {
 		serverPairTag := random.Name(random.WithMaxLength(10))
 		serviceBroker := helpers.CreateBroker(
 			helpers.BrokerWithPrefix("csb-mssql-fog-db"),
-			helpers.BrokerWithEnv(apps.EnvVar{Name: "MSSQL_DB_FOG_SERVER_PAIR_CREDS", Value: serverPairsConfig(serverPairTag)}),
 			// Disable brokerpak_updates due to bug - https://www.pivotaltracker.com/story/show/180586187
-			helpers.BrokerWithEnv(apps.EnvVar{Name: "BROKERPAK_UPDATES_ENABLED", Value: false}),
+			helpers.BrokerWithEnv(apps.EnvVar{Name: "MSSQL_DB_FOG_SERVER_PAIR_CREDS", Value: serverPairsConfig(serverPairTag)}, apps.EnvVar{Name: "BROKERPAK_UPDATES_ENABLED", Value: false}),
 		)
 		defer serviceBroker.Delete()
 
