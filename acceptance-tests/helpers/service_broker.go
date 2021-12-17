2c5146f394edaee5555568c38b4d2a6e2c80663b
George Blue <gblue@pivotal.io>
2021-12-17 18:08:39 +0000

    test(helper): fix default broker computation

--- acceptance-tests/helpers/service_broker.go
+++ acceptance-tests/helpers/service_broker.go
@@ -1,16 +1,39 @@
 package helpers
 
 import (
+	"acceptancetests/helpers/cf"
 	"fmt"
 	"os"
+
+	"code.cloudfoundry.org/jsonry"
+	. "github.com/onsi/gomega"
 )
 
+var defaultBrokerName string
+
 func DefaultBrokerName() string {
-	if v, ok := os.LookupEnv("BROKER_NAME"); ok {
-		return v
-	} else if v, ok := os.LookupEnv("USER"); ok {
-		return fmt.Sprintf("csb-%s", v)
-	} else {
-		panic("could not compute default broker name")
+	if defaultBrokerName != "" {
+		return defaultBrokerName
 	}
+
+	var receiver struct {
+		Names []string `jsonry:"resources.name"`
+	}
+	out, _ := cf.Run("curl", "/v3/service_brokers")
+	Expect(jsonry.Unmarshal([]byte(out), &receiver)).NotTo(HaveOccurred())
+
+	username := os.Getenv("USER")
+	for _, n := range receiver.Names {
+		if n == "broker-cf-test" {
+			defaultBrokerName = n
+			return n
+		}
+
+		if username != "" && n == fmt.Sprintf("csb-%s", username) {
+			defaultBrokerName = n
+			return n
+		}
+	}
+
+	panic("could not determine default broker name")
 }
