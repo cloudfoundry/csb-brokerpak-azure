97c9f96d179808e54bf735ea3c5131ab5474a18e
George Blue <blgm@users.noreply.github.com>
2022-01-26 17:37:21 +0000

    test: generated passwords should not start with hyphen (#171)
    [#181028147](https://www.pivotaltracker.com/story/show/181028147)

--- acceptance-tests/helpers/random/password.go
+++ acceptance-tests/helpers/random/password.go
@@ -8,17 +8,31 @@ import (
 	. "github.com/onsi/gomega"
 )
 
+var (
+	firstLetter      = regexp.MustCompile(`^[a-zA-Z]$`)
+	subsequentLetter = regexp.MustCompile(`^[~_.a-zA-Z0-9]$`)
+)
+
 func Password(opts ...Option) string {
 	length := cfg(append([]Option{WithMaxLength(24)}, opts...)).length
 	var s strings.Builder
+
+	s.WriteByte(byteMatching(firstLetter))
+
 	for s.Len() < length {
-		buf := make([]byte, 1)
-		_, err := rand.Read(buf)
-		Expect(err).NotTo(HaveOccurred())
-		if regexp.MustCompile(`[~_.a-zA-Z0-9]`).MatchString(string(buf)) {
-			s.WriteString(string(buf))
-		}
+		s.WriteByte(byteMatching(subsequentLetter))
 	}
 
 	return s.String()
+}
+
+func byteMatching(re *regexp.Regexp) byte {
+	buf := make([]byte, 1)
+	for {
+		_, err := rand.Read(buf)
+		Expect(err).NotTo(HaveOccurred())
+		if re.MatchString(string(buf)) {
+			return buf[0]
+		}
+	}
 }
