8c69441bd99cae5fa0a7f9593bc562d86809cfec
George Blue <gblue@pivotal.io>
2022-06-16 08:10:32 +0100

    test: revert: increase usage of shared helpers
    This change has caused the MS-SQL tests to consistently fail, so
    reverting in the expectation that it will fix things
    
    This reverts commit 4d68433f269ebe4f751308a49ff93226d3583e63.

--- acceptance-tests/mssql_server_pair_and_fog_db_test.go
+++ acceptance-tests/mssql_server_pair_and_fog_db_test.go
@@ -14,7 +14,7 @@ import (
 
 var _ = Describe("MSSQL Server Pair and Failover Group DB", Label("mssql-server-pair"), func() {
 	It("can be accessed by an app", func() {
-		serversConfig := serverpairs.NewDatabaseServerPair(metadata.ResourceGroup)
+		serversConfig := serverpairs.NewDatabaseServerPair(metadata)
 
 		By("Create CSB with server details")
 		serviceBroker := brokers.Create(
