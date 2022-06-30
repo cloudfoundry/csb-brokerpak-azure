edca115abf34611fcd8945d75cb95da674dbd860
James Norman <normanja@vmware.com>
2022-06-30 17:14:35 +0100

    chore: match project tf file structure (#264)

--- terraform/azure-mssql-db/provision/mssql-db-versions.tf
+++ terraform/azure-mssql-db/provision/mssql-db-versions.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "hashicorp/random"
       version = ">=3.3.1"
     }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=2.59.0"
+    }
   }
 }
 
