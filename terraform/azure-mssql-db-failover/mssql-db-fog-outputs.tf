44f7d98304f3aecc89f35124187ab73ca1a28b23
James Norman <normanja@vmware.com>
2022-07-05 15:18:45 +0100

    fix: use one() function to pass TF syntax check (#272)
    on versions of Terraform >0.12 the outputs were failing
    as they were being validated while the primary_db resource was
    still null. This change ensures that either a null value or correct
    value is returned, whereas previously we were indexing an empty object
    causing a failure.
    
    [#182635325](https://www.pivotaltracker.com/story/show/182635325)

--- terraform/azure-mssql-db-failover/mssql-db-fog-outputs.tf
+++ terraform/azure-mssql-db-failover/mssql-db-fog-outputs.tf
@@ -1,9 +1,9 @@
-output "sqldbName" { value = var.existing ? var.db_name : azurerm_mssql_database.primary_db[0].name }
-output "sqlServerName" { value = var.existing ? var.instance_name : azurerm_sql_failover_group.failover_group[0].name }
-output "sqlServerFullyQualifiedDomainName" { value = format("%s.database.windows.net", var.existing ? var.instance_name : azurerm_sql_failover_group.failover_group[0].name) }
-output "hostname" { value = format("%s.database.windows.net", var.existing ? var.instance_name : azurerm_sql_failover_group.failover_group[0].name) }
+output "sqldbName" { value = var.existing ? var.db_name : one(azurerm_mssql_database.primary_db[*].name) }
+output "sqlServerName" { value = var.existing ? var.instance_name : one(azurerm_sql_failover_group.failover_group[*].name) }
+output "sqlServerFullyQualifiedDomainName" { value = format("%s.database.windows.net", var.existing ? var.instance_name : one(azurerm_sql_failover_group.failover_group[*].name)) }
+output "hostname" { value = format("%s.database.windows.net", var.existing ? var.instance_name : one(azurerm_sql_failover_group.failover_group[*].name)) }
 output "port" { value = 1433 }
-output "name" { value = var.existing ? var.db_name : azurerm_mssql_database.primary_db[0].name }
+output "name" { value = var.existing ? var.db_name : one(azurerm_mssql_database.primary_db[*].name) }
 output "username" { value = var.server_credential_pairs[var.server_pair].admin_username }
 output "password" { value = var.server_credential_pairs[var.server_pair].admin_password }
 output "server_pair" { value = var.server_pair }
@@ -13,10 +13,10 @@ output "status" {
     data.azurerm_sql_server.secondary_sql_db_server.name, data.azurerm_sql_server.secondary_sql_db_server.id,
     var.azure_tenant_id,
     data.azurerm_sql_server.primary_sql_db_server.id) : format("created failover group %s (id: %s), primary db %s (id: %s) on server %s (id: %s), secondary db %s (id: %s/databases/%s) on server %s (id: %s) URL: https://portal.azure.com/#@%s/resource%s/failoverGroup",
-    azurerm_sql_failover_group.failover_group[0].name, azurerm_sql_failover_group.failover_group[0].id,
-    azurerm_sql_failover_group.failover_group[0].name, azurerm_mssql_database.primary_db[0].id,
+    one(azurerm_sql_failover_group.failover_group[*].name), one(azurerm_sql_failover_group.failover_group[*].id),
+    one(azurerm_sql_failover_group.failover_group[*].name), one(azurerm_mssql_database.primary_db[*].id),
     data.azurerm_sql_server.primary_sql_db_server.name, data.azurerm_sql_server.primary_sql_db_server.id,
-    azurerm_mssql_database.primary_db[0].name, data.azurerm_sql_server.secondary_sql_db_server.id, azurerm_mssql_database.primary_db[0].name,
+    one(azurerm_mssql_database.primary_db[*].name), data.azurerm_sql_server.secondary_sql_db_server.id, one(azurerm_mssql_database.primary_db[*].name),
     data.azurerm_sql_server.secondary_sql_db_server.name, data.azurerm_sql_server.secondary_sql_db_server.id,
     var.azure_tenant_id,
   data.azurerm_sql_server.primary_sql_db_server.id)
