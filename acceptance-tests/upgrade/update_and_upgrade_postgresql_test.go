85dfa7a71e4742505eed0b1ddc7b14189a95ff6b
James Norman <normanja@vmware.com>
2022-07-06 11:29:15 +0100

    wip: postgres tests
    [#182635325](https://www.pivotaltracker.com/story/show/182635325)

--- acceptance-tests/upgrade/update_and_upgrade_postgresql_test.go
+++ acceptance-tests/upgrade/update_and_upgrade_postgresql_test.go
@@ -12,88 +12,147 @@ import (
 
 var _ = Describe("UpgradePostgreSQLTest", Label("postgresql"), func() {
 	When("upgrading broker version", func() {
-		It("should continue to work", func() {
-			By("pushing latest released broker version")
-			serviceBroker := brokers.Create(
-				brokers.WithPrefix("csb-postgresql"),
-				brokers.WithSourceDir(releasedBuildDir),
-			)
-			defer serviceBroker.Delete()
+		Context("schema supported", Label("schema"), func() {
+			It("should continue to work", func() {
+				By("pushing latest released broker version")
+				serviceBroker := brokers.Create(
+					brokers.WithPrefix("csb-postgresql"),
+					brokers.WithSourceDir(releasedBuildDir),
+				)
+				defer serviceBroker.Delete()
 
-			By("creating a service")
-			serviceInstance := services.CreateInstance(
-				"csb-azure-postgresql",
-				"small",
-				services.WithBroker(serviceBroker),
-			)
-			defer serviceInstance.Delete()
+				By("creating a service")
+				serviceInstance := services.CreateInstance(
+					"csb-azure-postgresql",
+					"small",
+					services.WithBroker(serviceBroker),
+				)
+				defer serviceInstance.Delete()
 
-			By("pushing the unstarted app twice")
-			appOne := apps.Push(apps.WithApp(apps.PostgreSQL))
-			appTwo := apps.Push(apps.WithApp(apps.PostgreSQL))
-			defer apps.Delete(appOne, appTwo)
+				By("pushing the unstarted app twice")
+				appOne := apps.Push(apps.WithApp(apps.PostgreSQL))
+				appTwo := apps.Push(apps.WithApp(apps.PostgreSQL))
+				defer apps.Delete(appOne, appTwo)
 
-			By("binding to the apps")
-			bindingOne := serviceInstance.Bind(appOne)
-			bindingTwo := serviceInstance.Bind(appTwo)
+				By("binding to the apps")
+				bindingOne := serviceInstance.Bind(appOne)
+				bindingTwo := serviceInstance.Bind(appTwo)
 
-			By("starting the apps")
-			apps.Start(appOne, appTwo)
+				By("starting the apps")
+				apps.Start(appOne, appTwo)
 
-			By("creating a schema using the first app")
-			schema := random.Name(random.WithMaxLength(10))
-			appOne.PUT("", schema)
+				By("creating a schema using the first app")
+				schema := random.Name(random.WithMaxLength(10))
+				appOne.PUT("", schema)
 
-			By("setting a key-value using the first app")
-			keyOne := random.Hexadecimal()
-			valueOne := random.Hexadecimal()
-			appOne.PUT(valueOne, "%s/%s", schema, keyOne)
+				By("setting a key-value using the first app")
+				keyOne := random.Hexadecimal()
+				valueOne := random.Hexadecimal()
+				appOne.PUT(valueOne, "%s/%s", schema, keyOne)
 
-			By("getting the value using the second app")
-			got := appTwo.GET("%s/%s", schema, keyOne)
-			Expect(got).To(Equal(valueOne))
+				By("getting the value using the second app")
+				got := appTwo.GET("%s/%s", schema, keyOne)
+				Expect(got).To(Equal(valueOne))
 
-			By("pushing the development version of the broker")
-			serviceBroker.UpgradeBroker(developmentBuildDir)
+				By("pushing the development version of the broker")
+				serviceBroker.UpgradeBroker(developmentBuildDir)
 
-			By("upgrading service instance")
-			serviceInstance.Upgrade()
+				By("upgrading service instance")
+				serviceInstance.Upgrade()
 
-			By("checking previously written data still accessible")
-			got = appTwo.GET("%s/%s", schema, keyOne)
-			Expect(got).To(Equal(valueOne))
+				By("checking previously written data still accessible")
+				got = appTwo.GET("%s/%s", schema, keyOne)
+				Expect(got).To(Equal(valueOne))
 
-			By("updating the instance plan")
-			serviceInstance.Update("-p", "medium")
+				By("deleting bindings created before the upgrade")
+				bindingOne.Unbind()
+				bindingTwo.Unbind()
 
-			By("checking previously written data still accessible")
-			got = appTwo.GET("%s/%s", schema, keyOne)
-			Expect(got).To(Equal(valueOne))
+				By("creating new bindings and testing they still work")
+				serviceInstance.Bind(appOne)
+				serviceInstance.Bind(appTwo)
+				apps.Restage(appOne, appTwo)
 
-			By("deleting bindings created before the upgrade")
-			bindingOne.Unbind()
-			bindingTwo.Unbind()
+				By("checking previously written data still accessible")
+				got = appTwo.GET("%s/%s", schema, keyOne)
+				Expect(got).To(Equal(valueOne))
 
-			By("creating new bindings and testing they still work")
-			serviceInstance.Bind(appOne)
-			serviceInstance.Bind(appTwo)
-			apps.Restage(appOne, appTwo)
+				By("creating a schema using the first app")
+				schemaTwo := random.Name(random.WithMaxLength(10))
+				appOne.PUT("", schemaTwo)
 
-			By("checking previously written data still accessible")
-			got = appTwo.GET("%s/%s", schema, keyOne)
-			Expect(got).To(Equal(valueOne))
+				By("checking data can still be written and read")
+				keyTwo := random.Hexadecimal()
+				valueTwo := random.Hexadecimal()
+				appOne.PUT(valueTwo, "%s/%s", schemaTwo, keyTwo)
 
-			By("creating a schema using the first app")
-			schemaTwo := random.Name(random.WithMaxLength(10))
-			appOne.PUT("", schemaTwo)
+				got = appTwo.GET("%s/%s", schemaTwo, keyTwo)
+				Expect(got).To(Equal(valueTwo))
+			})
+		})
 
-			By("checking data can still be written and read")
-			keyTwo := random.Hexadecimal()
-			valueTwo := random.Hexadecimal()
-			appOne.PUT(valueTwo, "%s/%s", schemaTwo, keyTwo)
+		Context("schema not supported", Label("noschema"), func() {
+			It("should continue to work", func() {
+				By("pushing latest released broker version")
+				serviceBroker := brokers.Create(
+					brokers.WithPrefix("csb-postgresql"),
+					brokers.WithSourceDir(releasedBuildDir),
+				)
+				defer serviceBroker.Delete()
 
-			got = appTwo.GET("%s/%s", schemaTwo, keyTwo)
-			Expect(got).To(Equal(valueTwo))
+				By("creating a service")
+				serviceInstance := services.CreateInstance(
+					"csb-azure-postgresql",
+					"small",
+					services.WithBroker(serviceBroker),
+				)
+				defer serviceInstance.Delete()
+
+				By("pushing the unstarted app")
+				app := apps.Push(apps.WithApp(apps.PostgreSQL))
+				defer apps.Delete(app)
+
+				By("binding to the app")
+				binding := serviceInstance.Bind(app)
+
+				By("starting the app")
+				apps.Start(app)
+
+				By("setting a key-value")
+				keyOne := random.Hexadecimal()
+				valueOne := random.Hexadecimal()
+				const schema = "public"
+				app.PUT(valueOne, "%s/%s", schema, keyOne)
+
+				By("getting the value")
+				got := app.GET("%s/%s", schema, keyOne)
+				Expect(got).To(Equal(valueOne))
+
+				By("pushing the development version of the broker")
+				serviceBroker.UpgradeBroker(developmentBuildDir)
+
+				By("upgrading service instance")
+				serviceInstance.Upgrade()
+
+				By("checking previously written data still accessible")
+				got = app.GET("%s/%s", schema, keyOne)
+				Expect(got).To(Equal(valueOne))
+
+				By("deleting bindings created before the upgrade")
+				binding.Unbind()
+
+				By("creating new bindings and testing they still work")
+				serviceInstance.Bind(app)
+				apps.Restage(app)
+
+				By("checking data can still be written and read")
+				keyTwo := random.Hexadecimal()
+				valueTwo := random.Hexadecimal()
+				app.PUT(valueTwo, "%s/%s", schema, keyTwo)
+
+				got = app.GET("%s/%s", schema, keyTwo)
+				Expect(got).To(Equal(valueTwo))
+			})
 		})
 	})
 })
