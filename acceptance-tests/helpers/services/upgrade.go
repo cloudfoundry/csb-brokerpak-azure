48a5e8fefd4077a29a28d392e19be0b97e36a206
George Blue <blgm@users.noreply.github.com>
2022-07-15 14:18:20 +0100

    test(MongoDB): check bindings work after upgrade failure (#290)
    [#182413672](https://www.pivotaltracker.com/story/show/182413672)

--- acceptance-tests/helpers/services/upgrade.go
+++ acceptance-tests/helpers/services/upgrade.go
@@ -41,3 +41,26 @@ func (s *ServiceInstance) UpgradeAvailable() bool {
 	Expect(json.Unmarshal([]byte(out), &receiver)).NotTo(HaveOccurred())
 	return receiver.UpgradeAvailable
 }
+
+func (s *ServiceInstance) UpgradeExpectFailure() {
+	Expect(s.UpgradeAvailable()).To(BeTrue(), "service instance does not have an upgrade available")
+
+	var command []string
+	switch cf.Version() {
+	case cf.VersionV8:
+		command = []string{"upgrade-service", s.Name, "--force"}
+	default:
+		command = []string{"update-service", s.Name, "--upgrade", "--force"}
+	}
+
+	session := cf.Start(command...)
+	Eventually(session).WithTimeout(asyncCommandTimeout).Should(Exit(0))
+
+	Eventually(func() string {
+		out, _ := cf.Run("service", s.Name)
+		Expect(out).NotTo(MatchRegexp(`status:\s+update succeeded`))
+		return out
+	}).WithTimeout(operationTimeout).WithPolling(pollingInterval).Should(MatchRegexp(`status:\s+update failed`))
+
+	Expect(s.UpgradeAvailable()).To(BeTrue(), "service instance should still have an upgrade available after failure to upgrade")
+}
--- acceptance-tests/upgrade/update_and_upgrade_mongodb_test.go
+++ acceptance-tests/upgrade/update_and_upgrade_mongodb_test.go
@@ -11,7 +11,7 @@ import (
 )
 
 var _ = Describe("UpgradeMongoTest", Label("mongodb"), func() {
-	When("upgrading broker version", func() {
+	When("upgrading broker version", Label("succeeding"), func() {
 		It("should continue to work", func() {
 			By("pushing latest released broker version")
 			serviceBroker := brokers.Create(
@@ -94,6 +94,61 @@ var _ = Describe("UpgradeMongoTest", Label("mongodb"), func() {
 			By("getting the document using the second app")
 			got = appTwo.GET("%s/%s/%s", databaseName, collectionName, documentNameTwo)
 			Expect(got).To(Equal(documentDataTwo))
+		})
+	})
+
+	When("upgrading broker version", Label("failing"), func() {
+		It("should retain data and bindings", func() {
+			By("pushing latest released broker version")
+			serviceBroker := brokers.Create(
+				brokers.WithPrefix("csb-mongodb"),
+				brokers.WithSourceDir(releasedBuildDir),
+			)
+			defer serviceBroker.Delete()
+
+			By("creating a service instance")
+			databaseName := random.Name(random.WithPrefix("database"))
+			collectionName := random.Name(random.WithPrefix("collection"))
+			serviceInstance := services.CreateInstance(
+				"csb-azure-mongodb",
+				"small",
+				services.WithBroker(serviceBroker),
+				services.WithParameters(map[string]any{
+					"db_name":         databaseName,
+					"collection_name": collectionName,
+					"shard_key":       "_id",
+				}),
+			)
+			defer serviceInstance.Delete()
+
+			By("pushing the unstarted app")
+			app := apps.Push(apps.WithApp(apps.MongoDB))
+			defer apps.Delete(app)
+
+			By("binding the app")
+			serviceInstance.Bind(app)
+
+			By("starting the app")
+			apps.Start(app)
+
+			By("creating a document")
+			documentNameOne := random.Hexadecimal()
+			documentDataOne := random.Hexadecimal()
+			app.PUT(documentDataOne, "%s/%s/%s", databaseName, collectionName, documentNameOne)
+
+			By("getting the document")
+			got := app.GET("%s/%s/%s", databaseName, collectionName, documentNameOne)
+			Expect(got).To(Equal(documentDataOne))
+
+			By("pushing the development version of the broker")
+			serviceBroker.UpgradeBroker(developmentBuildDir)
+
+			By("upgrading service instance")
+			serviceInstance.UpgradeExpectFailure()
+
+			By("checking previous data still accessible")
+			got = app.GET("%s/%s/%s", databaseName, collectionName, documentNameOne)
+			Expect(got).To(Equal(documentDataOne))
 		})
 	})
 })
