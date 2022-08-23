5740dc6041de04d9e2945ac18a9336a3e9b79f9c
Andrea Zucchini <zandrea@vmware.com>
2022-08-23 09:57:09 +0100

    fix: a problem when logging the secret value in terraform-provider-csbmssqldbrunfailover (#317)
    * fix: regex for azure secret in terraform-provider-csbmssqldbrunfailover
    
    We also fix a problem when logging the value of the secret.
    
    [#183032358](https://www.pivotaltracker.com/story/show/183032358)
    
    * fix: add flexibility when validating parameters
    
    [#183032253](https://www.pivotaltracker.com/story/show/183032253)
    
    * docs: improve release notes
    
    [#183032253](https://www.pivotaltracker.com/story/show/183032253)
    
    Co-authored-by: James Norman <normanja@vmware.com>

--- docs/draft-release-notes.md
+++ docs/draft-release-notes.md
@@ -3,3 +3,5 @@
 ### New feature:
 
 ### Fix:
+- **Failover service client secret parameter is no longer exposed:** The value of the Client Secret is considered a sensible field. We avoid logging its value in plain text in our custom terraform provider csbmssqldbrunfailover.
+- **Failover service parameters validation is more flexible:** Previously, the validation of the parameters was carried out in a more restrictive way adding rigidity to the admitted values. With this change, the main validation is on the supplier side, and we only check if the field is empty.
--- providers/terraform-provider-csbmssqldbrunfailover/csbmssqldbrunfailover/config.go
+++ providers/terraform-provider-csbmssqldbrunfailover/csbmssqldbrunfailover/config.go
@@ -1,33 +1,15 @@
 package csbmssqldbrunfailover
 
 import (
-	"regexp"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var (
-	identifierRegexp   = regexp.MustCompile(`^[\w_.-]{1,64}$`)
-	clientSecretRegexp = regexp.MustCompile(`^[\w_.-]{1,96}$`)
-)
-
-// getIdentifier gets a string configuration value and validates that it's
-// a valid identifier
+// getIdentifier gets a string configuration value and validates that it's a valid identifier
 func getIdentifier(d *schema.ResourceData, key string) (string, diag.Diagnostics) {
-	// We rely on Terraform to supply the correct types, and it's ok panic if this contract is broken
 	s := d.Get(key).(string)
-	if !identifierRegexp.MatchString(s) {
-		return "", diag.Errorf("invalid value %q for identifier %q, validation expression is: %s", s, key, identifierRegexp.String())
-	}
-
-	return s, nil
-}
-
-func getClientSecret(d *schema.ResourceData) (string, diag.Diagnostics) {
-	s := d.Get(azureClientSecretKey).(string)
-	if !clientSecretRegexp.MatchString(s) {
-		return "", diag.Errorf("invalid value %q for identifier %q, validation expression is: %s", s, azureClientSecretKey, clientSecretRegexp.String())
+	if s == "" {
+		return "", diag.Errorf("empty value for identifier %q", key)
 	}
 
 	return s, nil
