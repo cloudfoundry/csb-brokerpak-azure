cf91c7447093a53ecd980fa95436bf74f155fb3e
Konstantin <34882943+nouseforaname@users.noreply.github.com>
2022-10-11 16:20:34 +0200

    feat: mark sensitive values in var inputs and outputs (#356)
    terraform will display sensitive values in debug logs if not
    instructured to redact them. This adds redaction to possible sources for
    credential leaks in the tf input / output data.
    
    [ #183474479 ]

--- terraform/azure-mssql-db/bind/mssql-bind-variables.tf
+++ terraform/azure-mssql-db/bind/mssql-bind-variables.tf
@@ -16,6 +16,12 @@ variable "mssql_db_name" { type = string }
 variable "mssql_hostname" { type = string }
 variable "mssql_port" { type = number }
 variable "admin_username" { type = string }
-variable "admin_password" { type = string }
+variable "admin_password" {
+  type = string
+  sensitive = true
+}
 variable "server" { type = string }
-variable "server_credentials" { type = map(any) }
+variable "server_credentials" { 
+  type = map(any)
+  sensitive = true
+}
--- terraform/azure-storage/account-provision.tf
+++ terraform/azure-storage/account-provision.tf
@@ -18,10 +18,22 @@ variable "replication_type" { type = string }
 variable "location" { type = string }
 variable "labels" { type = map(any) }
 variable "resource_group" { type = string }
-variable "azure_tenant_id" { type = string }
-variable "azure_subscription_id" { type = string }
-variable "azure_client_id" { type = string }
-variable "azure_client_secret" { type = string }
+variable "azure_tenant_id" {
+  type = string
+  sensitive = true
+}
+variable "azure_subscription_id" {
+  type = string
+  sensitive = true
+}
+variable "azure_client_id" {
+  type = string
+  sensitive = true
+}
+variable "azure_client_secret" {
+  type = string
+  sensitive = true
+}
 # variable authorized_network {type = string}
 variable "skip_provider_registration" { type = bool }
 variable "authorized_networks" { type = list(string) }
