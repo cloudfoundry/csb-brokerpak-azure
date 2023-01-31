b696abec984b9c1ffcee0303a5f179f8e743d77c
Konstantin <34882943+nouseforaname@users.noreply.github.com>
2023-01-31 11:37:02 +0100

    Add test providing settings for server creds via config file (#351)
    currently providing pre created MSSQL to the broker to use for the
    mSSQL-DB service is not covered by tests. We want to make sure that
    either setting (env, yaml) works correctly.
    
    Note:
    the current parsing in the broker itself for the config file requires a
    `string` value in the yaml
    
    ```
    azure.mssql_db_server_creds: '{ "name": {...}}'
    ```
    
    providing
    
    ```
    azure.mssql_db_server_creds: { "name": {...}}
    azure.mssql_db_server_creds:
      name:
        ...
    ```
    will not be correctly unmarshalled by the broker
    
    Co-authored-by: Konstantin Semenov <jhvhs@users.noreply.github.com>
    Co-authored-by: George Blue <blgm@users.noreply.github.com>

--- acceptance-tests/helpers/brokers/update.go
+++ acceptance-tests/helpers/brokers/update.go
@@ -32,6 +32,21 @@ func (b *Broker) UpdateEnv(env ...apps.EnvVar) {
 
 	cf.Run("update-service-broker", b.Name, b.username, b.password, b.app.URL)
 }
+func (b *Broker) UpdateConfig(config map[string]interface{}) {
+	b.app.Push(
+		apps.WithName(b.Name),
+		apps.WithYAMLFile("config.yml", config),
+		apps.WithManifest(newManifest(
+			withName(b.Name),
+			withEnv(b.env()...),
+			withCustomStartCommand("./cloud-service-broker --config config.yml serve"),
+		)),
+	)
+
+	b.app.CleanFileFromAppDir("config.yml")
+
+	b.app.Start()
+}
 
 func (b *Broker) UpdateEncryptionSecrets(secrets ...EncryptionSecret) {
 	WithEncryptionSecrets(secrets...)
