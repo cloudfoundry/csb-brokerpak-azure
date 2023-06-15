8a09f2f81d6c696abb7ce0f186e9266a3051f28c
Marcela Campo <mcampo@pivotal.io>
2023-06-15 15:07:56 +0100

    tests: add test for upgrades passing through an intermediate version
    [#183376494](https://www.pivotaltracker.com/story/show/183376494)

---
+++ acceptance-tests/upgrade/multi_step_upgrade_mssql_db_test.go
@@ -0,0 +1,125 @@
+package upgrade_test
+
+import (
+	"csbbrokerpakazure/acceptance-tests/helpers/apps"
+	"csbbrokerpakazure/acceptance-tests/helpers/brokers"
+	"csbbrokerpakazure/acceptance-tests/helpers/random"
+	"csbbrokerpakazure/acceptance-tests/helpers/services"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("MultiStepUpgradeMssqlDBTest", Label("mssql-db", "multi-step"), func() {
+	When("upgrading broker version", func() {
+		It("should continue to work", func() {
+			By("pushing latest released broker version")
+			serviceBroker := brokers.Create(
+				brokers.WithPrefix("csb-srvdb"),
+				brokers.WithSourceDir(releasedBuildDir),
+			)
+			defer serviceBroker.Delete()
+
+			By("creating a service")
+			serverConfig := newDatabaseServer()
+			serverInstance := services.CreateInstance(
+				"csb-azure-mssql-server",
+				"standard",
+				services.WithBroker(serviceBroker),
+				services.WithParameters(serverConfig),
+			)
+			defer serverInstance.Delete()
+
+			By("reconfiguring the CSB with DB server details")
+			serverTag := random.Name(random.WithMaxLength(10))
+			serviceBroker.UpdateEnv(apps.EnvVar{Name: "MSSQL_DB_SERVER_CREDS", Value: serverConfig.serverDetails(serverTag)})
+
+			By("creating a database in the server")
+			dbInstance := services.CreateInstance(
+				"csb-azure-mssql-db",
+				"small",
+				services.WithBroker(serviceBroker),
+				services.WithParameters(map[string]string{"server": serverTag}),
+			)
+			defer dbInstance.Delete()
+
+			By("pushing the unstarted app twice")
+			appOne := apps.Push(apps.WithApp(apps.MSSQL))
+			appTwo := apps.Push(apps.WithApp(apps.MSSQL))
+			defer apps.Delete(appOne, appTwo)
+
+			By("binding to the apps")
+			bindingOne := dbInstance.Bind(appOne)
+			bindingTwo := dbInstance.Bind(appTwo)
+
+			By("starting the apps")
+			apps.Start(appOne, appTwo)
+
+			By("creating a schema using the first app")
+			schema := random.Name(random.WithMaxLength(10))
+			appOne.PUT("", schema)
+
+			By("setting a key-value using the first app")
+			keyOne := random.Hexadecimal()
+			valueOne := random.Hexadecimal()
+			appOne.PUT(valueOne, "%s/%s", schema, keyOne)
+
+			By("getting the value using the second app")
+			got := appTwo.GET("%s/%s", schema, keyOne)
+			Expect(got).To(Equal(valueOne))
+
+			By("Performing all intermediate upgrades")
+			for _, brokerDir := range intermediateBuildDirs {
+				By("pushing the next version of the broker")
+				serviceBroker.UpgradeBroker(brokerDir)
+				By("upgrading service instance")
+				dbInstance.Upgrade()
+				serverInstance.Upgrade()
+			}
+
+			By("pushing the development version of the broker")
+			serviceBroker.UpgradeBroker(developmentBuildDir)
+			By("upgrading service instance")
+			dbInstance.Upgrade()
+			serverInstance.Upgrade()
+
+			By("checking previously created data still accessible")
+			got = appTwo.GET("%s/%s", schema, keyOne)
+			Expect(got).To(Equal(valueOne))
+
+			By("updating the instance plan")
+			dbInstance.Update("-p", "medium")
+
+			By("checking previously created data still accessible")
+			got = appTwo.GET("%s/%s", schema, keyOne)
+			Expect(got).To(Equal(valueOne))
+
+			By("dropping the schema used to allow us to unbind")
+			appOne.DELETE(schema)
+
+			By("deleting bindings created before the upgrade")
+			bindingOne.Unbind()
+			bindingTwo.Unbind()
+
+			By("creating new bindings")
+			dbInstance.Bind(appOne)
+			dbInstance.Bind(appTwo)
+			apps.Restage(appOne, appTwo)
+
+			By("creating a schema using the first app")
+			schema = random.Name(random.WithMaxLength(10))
+			appOne.PUT("", schema)
+
+			By("checking data can still be written and read")
+			keyTwo := random.Hexadecimal()
+			valueTwo := random.Hexadecimal()
+			appOne.PUT(valueTwo, "%s/%s", schema, keyTwo)
+
+			got = appTwo.GET("%s/%s", schema, keyTwo)
+			Expect(got).To(Equal(valueTwo))
+
+			By("dropping the schema used to allow us to unbind")
+			appOne.DELETE(schema)
+		})
+	})
+})
--- acceptance-tests/upgrade/upgrade_suite_test.go
+++ acceptance-tests/upgrade/upgrade_suite_test.go
@@ -2,6 +2,7 @@ package upgrade_test
 
 import (
 	"flag"
+	"strings"
 	"testing"
 
 	"csbbrokerpakazure/acceptance-tests/helpers/environment"
@@ -11,14 +12,19 @@ import (
 )
 
 var (
-	developmentBuildDir string
-	releasedBuildDir    string
-	metadata            environment.Metadata
+	developmentBuildDir   string
+	releasedBuildDir      string
+	intermediateBuildDirs []string
+	metadata              environment.Metadata
 )
 
 func init() {
+	var intermediateBuildDirsFlag string
 	flag.StringVar(&releasedBuildDir, "releasedBuildDir", "../../../azure-released", "location of released version of built broker and brokerpak")
 	flag.StringVar(&developmentBuildDir, "developmentBuildDir", "../../dev-release", "location of development version of built broker and brokerpak")
+	flag.StringVar(&intermediateBuildDirsFlag, "intermediateBuildDirs", "", "comma separated locations of intermediate versions of built broker and brokerpak")
+
+	intermediateBuildDirs = strings.Split(intermediateBuildDirsFlag, ",")
 }
 
 var _ = BeforeSuite(func() {
