3eaa0bca874307ca5da794f531eb4d9fa44b9876
Andrea Zucchini <zandrea@vmware.com>
2023-11-20 14:18:04 +0000

    chore: format code

--- terraform/azure-eventhubs/azure-eventhubs-bind.tf
+++ terraform/azure-eventhubs/azure-eventhubs-bind.tf
@@ -47,7 +47,7 @@ data "azurerm_eventhub_namespace" "ns" {
 }
 
 output "event_hub_connection_string" {
-  value = "${data.azurerm_eventhub_namespace.ns.default_primary_connection_string};EntityPath=${var.eventhub_name}"
+  value     = "${data.azurerm_eventhub_namespace.ns.default_primary_connection_string};EntityPath=${var.eventhub_name}"
   sensitive = true
 }
 
@@ -56,7 +56,7 @@ output "event_hub_name" {
 }
 
 output "namespace_connection_string" {
-  value = data.azurerm_eventhub_namespace.ns.default_primary_connection_string
+  value     = data.azurerm_eventhub_namespace.ns.default_primary_connection_string
   sensitive = true
 }
 
