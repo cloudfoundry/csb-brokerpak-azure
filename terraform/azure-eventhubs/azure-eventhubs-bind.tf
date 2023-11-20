b1cb90b3b9480e57f1cc82c2bdce3d783f32d32d
Andrea Zucchini <zandrea@vmware.com>
2023-11-20 14:13:55 +0000

    fix: [eventhubs] flag sensitive output values
    [#186442543](https://www.pivotaltracker.com/story/show/186442543)

--- terraform/azure-eventhubs/azure-eventhubs-bind.tf
+++ terraform/azure-eventhubs/azure-eventhubs-bind.tf
@@ -48,6 +48,7 @@ data "azurerm_eventhub_namespace" "ns" {
 
 output "event_hub_connection_string" {
   value = "${data.azurerm_eventhub_namespace.ns.default_primary_connection_string};EntityPath=${var.eventhub_name}"
+  sensitive = true
 }
 
 output "event_hub_name" {
@@ -56,6 +57,7 @@ output "event_hub_name" {
 
 output "namespace_connection_string" {
   value = data.azurerm_eventhub_namespace.ns.default_primary_connection_string
+  sensitive = true
 }
 
 output "namespace_name" {
