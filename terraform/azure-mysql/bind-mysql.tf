295134a6b16689e7bb4735b33cd511cfdab26265
Marcela Campo <mcampo@pivotal.io>
2023-11-21 11:10:39 +0000

    chore: flag sensitive values in terraform outpus
    [#186442543](https://www.pivotaltracker.com/story/show/186442543)

--- terraform/azure-mysql/bind-mysql.tf
+++ terraform/azure-mysql/bind-mysql.tf
@@ -68,6 +68,7 @@ output "uri" {
     var.mysql_hostname,
     var.mysql_port,
   var.mysql_db_name)
+  sensitive = true
 }
 output "jdbcUrl" {
   value = format("jdbc:%s://%s:%s/%s?user=%s\u0026password=%s\u0026verifyServerCertificate=true\u0026useSSL=%v\u0026requireSSL=%v\u0026serverTimezone=GMT",
@@ -79,4 +80,5 @@ output "jdbcUrl" {
     random_password.password.result,
     var.use_tls,
   var.use_tls)
+  sensitive = true
 }  
