67940ae74ed01b8024c732efbf566f60c3aaf980
Felisia Martini <fmartini@pivotal.io>
2024-04-10 10:45:32 +0100

    Update cyrilgdn/postgresql provider hostname to be TF registry

--- terraform/azure-postgres/bind/versions.tf
+++ terraform/azure-postgres/bind/versions.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     postgresql = {
-      source  = "cyrilgdn/postgresql"
+      source  = "registry.terraform.io/cyrilgdn/postgresql"
       version = ">=1.16.0"
     }
     random = {
