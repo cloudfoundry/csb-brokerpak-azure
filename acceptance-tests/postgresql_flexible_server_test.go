ed6c9d51ff59230f77e5570458d8b96ea736eb73
Felisia Martini <fmartini@pivotal.io>
2024-05-22 15:12:00 +0100

    Add acceptance test

---
+++ acceptance-tests/postgresql_flexible_server_test.go
@@ -0,0 +1,53 @@
+package acceptance_test
+
+import (
+	"csbbrokerpakazure/acceptance-tests/helpers/apps"
+	"csbbrokerpakazure/acceptance-tests/helpers/matchers"
+	"csbbrokerpakazure/acceptance-tests/helpers/random"
+	"csbbrokerpakazure/acceptance-tests/helpers/services"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("PostgreSQL Fleaxible Server", Label("postgresql-flexible"), func() {
+	It("can be accessed by an app", func() {
+		By("creating a service instance")
+		serviceInstance := services.CreateInstance(
+			"csb-azure-postgresql-flexible-server",
+			"default",
+		)
+		defer serviceInstance.Delete()
+
+		By("pushing the unstarted app twice")
+		appOne := apps.Push(apps.WithApp(apps.PostgreSQL))
+		appTwo := apps.Push(apps.WithApp(apps.PostgreSQL))
+		defer apps.Delete(appOne, appTwo)
+
+		By("binding the apps to the service instance")
+		binding := serviceInstance.Bind(appOne)
+		serviceInstance.Bind(appTwo)
+
+		By("starting the apps")
+		apps.Start(appOne, appTwo)
+
+		By("checking that the app environment has a credhub reference for credentials")
+		Expect(binding.Credential()).To(matchers.HaveCredHubRef)
+
+		By("creating a schema using the first app")
+		schema := random.Name(random.WithMaxLength(10))
+		appOne.PUT("", schema)
+
+		By("setting a key-value using the first app")
+		key := random.Hexadecimal()
+		value := random.Hexadecimal()
+		appOne.PUT(value, "%s/%s", schema, key)
+
+		By("getting the value using the second app")
+		got := appTwo.GET("%s/%s", schema, key)
+		Expect(got).To(Equal(value))
+
+		By("dropping the schema using the first app")
+		appOne.DELETE(schema)
+	})
+})
