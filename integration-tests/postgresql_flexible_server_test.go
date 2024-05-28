0fae298c5cd23b966f44d4c11d5562ff082dcacc
Felisia Martini <fmartini@pivotal.io>
2024-05-28 14:07:26 +0100

    Fix formatting

--- integration-tests/postgresql_flexible_server_test.go
+++ integration-tests/postgresql_flexible_server_test.go
@@ -2,6 +2,7 @@ package integration_test
 
 import (
 	"fmt"
+
 	testframework "github.com/cloudfoundry/cloud-service-broker/v2/brokerpaktestframework"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
