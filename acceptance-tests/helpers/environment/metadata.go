ac0650a1bbdb1e1d5edd62bbb952ae1e901d81f5
Felisia Martini <fmartini@pivotal.io>
2024-06-25 10:53:23 +0100

    chore: Remove unused legacy broker MASB metadata
    [#187846697](https://www.pivotaltracker.com/story/show/187846697)

--- acceptance-tests/helpers/environment/metadata.go
+++ acceptance-tests/helpers/environment/metadata.go
@@ -9,15 +9,7 @@ import (
 )
 
 type Metadata struct {
-	ResourceGroup             string `jsonry:"name"`
-	PreProvisionedSQLUsername string `jsonry:"masb_config.pre_provisioned_sql.username"`
-	PreProvisionedSQLPassword string `jsonry:"masb_config.pre_provisioned_sql.password"`
-	PreProvisionedSQLServer   string `jsonry:"masb_config.pre_provisioned_sql.server_name"`
-	PreProvisionedSQLLocation string `jsonry:"masb_config.location"`
-	PreProvisionedFOGUsername string `jsonry:"masb_config.pre_provisioned_fog_sql.username"`
-	PreProvisionedFOGPassword string `jsonry:"masb_config.pre_provisioned_fog_sql.password"`
-	PreProvisionedFOGServer   string `jsonry:"masb_config.pre_provisioned_fog_sql.server_name"`
-	PreProvisionedFOGLocation string `jsonry:"masb_config.pre_provisioned_fog_sql.location"`
+	ResourceGroup string `jsonry:"name"`
 }
 
 func ReadMetadata() Metadata {
@@ -30,13 +22,5 @@ func ReadMetadata() Metadata {
 	var metadata Metadata
 	Expect(jsonry.Unmarshal(contents, &metadata)).NotTo(HaveOccurred())
 	Expect(metadata.ResourceGroup).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedSQLUsername).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedSQLPassword).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedSQLServer).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedSQLLocation).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedFOGUsername).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedFOGPassword).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedFOGServer).NotTo(BeEmpty())
-	Expect(metadata.PreProvisionedFOGLocation).NotTo(BeEmpty())
 	return metadata
 }
