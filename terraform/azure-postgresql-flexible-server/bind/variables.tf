89888c24d3b826682b355338848551309c88a872
Andrea Zucchini <zandrea@vmware.com>
2024-09-02 15:26:19 +0100

    feat!: Azure Postgres Flexible Server retirement

--- terraform/azure-postgresql-flexible-server/bind/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "db_name" { type = string }
-variable "hostname" { type = string }
-variable "port" { type = number }
-variable "admin_username" { type = string }
-variable "admin_password" {
-  type      = string
-  sensitive = true
-}
--- terraform/azure-postgresql-flexible-server/provision/data.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  resource_group = length(var.resource_group) == 0 ? format("rg-%s", var.instance_name) : var.resource_group
-}
