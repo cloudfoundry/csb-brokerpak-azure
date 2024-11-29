1b3675d887f6b2f46c02989e6ae12f9a74489b82
George Blue <blgm@users.noreply.github.com>
2024-11-29 21:09:12 +0000

    build(deps): bump Azure terraform provider to v4 (#888)

--- terraform/azure-redis/provision/versions.tf
+++ terraform/azure-redis/provision/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "registry.terraform.io/hashicorp/azurerm"
-      version = "~> 3"
+      version = "~> 4"
     }
     random = {
       source  = "registry.terraform.io/hashicorp/random"
