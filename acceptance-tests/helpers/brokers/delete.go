7639f81e4ab34627cf0ec723cc001f6f95ed593a
George Blue <blgm@users.noreply.github.com>
2025-01-31 09:22:16 +0000

    test: fix mssql_db_failover_existing test (#925)
    - As well as testing the "existing" plan, it used another plan too,
      and therefore needed changes because of #922
    - Also explicitly frees backing database on broker delete, which will
      hopefully reduce leaking of resources

--- acceptance-tests/helpers/brokers/delete.go
+++ acceptance-tests/helpers/brokers/delete.go
@@ -1,8 +1,13 @@
 package brokers
 
-import "csbbrokerpakazure/acceptance-tests/helpers/cf"
+import (
+	"csbbrokerpakazure/acceptance-tests/helpers/cf"
+)
 
 func (b *Broker) Delete() {
+	// This is implicit when deleting the app, but sometimes that fails, so this ensures the resource is freed
+	cf.Run("unbind-service", b.Name, "csb-sql")
+
 	cf.Run("delete-service-broker", b.Name, "-f")
 	b.app.Delete()
 }
